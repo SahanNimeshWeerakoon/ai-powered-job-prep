7f795685fb6b7f7a21de5382d2cafa2821b0db0d
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2025-11-01 12:33:03 +0530

    type safe the env variables

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,24 @@
+import arcjet, { detectBot, shield, slidingWindow } from '@arcjet/next'
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { env } from './data/env/server';
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/'])
 
+const aj = arcjet({
+  key: env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({ mode: "LIVE", allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR", "CATEGORY:PREVIEW"] }),
+    slidingWindow({ mode: "LIVE", interval: "1m", max: 100 })
+  ]
+});
+
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect()
-  }
+  const decisition = await aj.protect(req);
+  
+  if (decisition.isDenied()) return new Response(null, { status: 403 });
+  
+  if (!isPublicRoute(req)) await auth.protect()
 })
 
 export const config = {
